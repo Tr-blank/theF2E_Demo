6e231af7466e3c9a5a41ac313d89845d35aaa630
Tr-blank <blank0129@gmail.com>
2018-06-05 14:20:18 +0800

    firse
    text

---
+++ precache-manifest.921c5b3e8d7463d1dad9c3a9295383b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3b926bf1cbb574880d8",
+    "url": "/js/chunk-vendors.c3b926bf.js"
+  },
+  {
+    "revision": "9e9fe765da3677571e6f",
+    "url": "/js/app.9e9fe765.js"
+  },
+  {
+    "revision": "3061b02cfd3982ee2db5ec1921df15aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e9fe765da3677571e6f",
+    "url": "/css/app.4473f4b7.css"
+  }
+];
